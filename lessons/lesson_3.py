7f5ae7c28a640aa43076207a354d969a2789bd07
turaev_sv <turaev.prof@gmail.com>
2026-07-21 22:08:32 +0300

    new file with homework

---
+++ lessons/lesson_3.py
@@ -0,0 +1,10 @@
+# Python initiate
+strike="======================================================================"
+name="Сергей"
+city="Пенза"
+print(strike)
+print("Имя:",name,'\n'"\nГород проживания:",city)
+print("\nЯчейка в памяти на которую ссылается переменная name:", id(name))
+print("\nТип данных переменной city:", type(city))
+print(strike)
+
