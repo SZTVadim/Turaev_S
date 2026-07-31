a441f411bfc8ea255697462cf6b45ff2e58327cb
turaev_sv <turaev.prof@gmail.com>
2026-07-31 17:45:53 +0300

    ДЗ_5

---
+++ Homeworks/Homework_5/h_work_5.py
@@ -0,0 +1,26 @@
+fruits = ["яблоко"]
+print(fruits)
+fruits.extend(["апельсин", "груша"])
+print(fruits)
+fruits.append("банан")
+print(fruits)
+fruits.insert(1,"виноград")
+print(fruits,"\n")
+
+fruits = ["яблоко", "банан", "апельсин", "банан"]
+print(fruits)
+fruits.remove("банан")
+print(fruits)
+end_fruits = fruits.pop()
+print(end_fruits, "\n")
+
+fruits = ["яблоко", "банан", "апельсин", "банан"]
+print(fruits.index('банан'))
+print(fruits.count('банан'), "\n")
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
