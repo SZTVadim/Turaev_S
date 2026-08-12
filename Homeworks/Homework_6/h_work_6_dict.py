0c96fd3b118e9ee07f57f2465817ca8e13416427
turaev_sv <turaev.prof@gmail.com>
2026-08-12 17:30:48 +0300

    Hw_6

---
+++ Homeworks/Homework_6/h_work_6_dict.py
@@ -0,0 +1,37 @@
+student = {
+    "name": 'ivan',
+    "age": 20,
+    "course": 2,
+    "city": 'Moscow'
+}
+
+print(student.keys())
+print(student.values(),"\n")
+
+for key, value in student.items():
+    print(key, value)
+for key, value in student.items():
+    print(value)
+
+student_1 = {
+    "name": 'ivan',
+    "age": 20,
+    "course": 2
+}
+
+student_2 = {
+    "name": 'maria',
+    "age": 21,
+    "city": 'Saints_petersburg'
+}
+
+student_1.update(student_2)
+print(student_1, "\n")
+
+student_3 = student_1.copy()
+student_3.update(student_2)
+print(student_3, "\n")
+
+print(student_1)
+print(student_2)
+print(student_3)
---
+++ Homeworks/Homework_6/h_work_6_set.py
@@ -0,0 +1,57 @@
+fruits = {"яблоко", "банан"}
+fruits.add("апельсин")
+print(fruits)
+fr_2 = {"груша", "виноград"}
+fruits.update(fr_2)
+print(fruits)
+fruits.discard("банан")
+print(fruits)
+fruits.discard("киви")
+print(fruits)
+# fruits.remove("киви")
+# print(fruits)
+rem_el = fruits.pop()
+print(rem_el, "\n")
+
+coord = (10, 20, 30, 20, 10, 20, 40)
+print(coord)
+print(coord[1])
+print(coord[-1:])
+print(coord[1:5])
+print("30" in coord)
+print(coord.index(20))
+print(coord.count(20))
+print(coord.count(50))
+print(len(coord))
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+numbers = [10, 20, 30, 40, 50]
+tup3 = tuple1 + tuple2
+print(tup3)
+tup4= tuple1 * 3
+print(tup4)
+a, b, c = tuple1
+print(a)
+print(b)
+print(c, "\n")
+
+numbers_tuple = tuple(numbers)
+first = numbers_tuple[0:1]
+middle = numbers_tuple[1:4]
+last = numbers_tuple[-1:]
+print(first)
+print(middle)
+print(last, "\n")
+
+numbers_tuple = tuple(numbers)
+print(numbers_tuple, "\n")
+
+even = tuple(x for x in range(11) if x % 2 == 0)
+print(even, "\n")
+
+sqrt = tuple (x ** 2 for x in range(1,6))
+print(sqrt, "\n")
+
+single_tuple = 42
+print(single_tuple, "\n")
