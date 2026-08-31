d815f3f635eefe45e177ff03bde4c040cf60d701
turaev_sv <turaev.prof@gmail.com>
2026-08-31 21:13:07 +0300

    исправленный

--- Homeworks/Homework_6/h_work_6_dict.py
+++ Homeworks/Homework_6/h_work_6_dict.py
@@ -10,7 +10,7 @@ print(student.values(), "\n")
 
 for key, value in student.items():
     print(key, value)
-for _, value in student.items():
+for value in student.items():
     print(value)
 
 student_1 = {
