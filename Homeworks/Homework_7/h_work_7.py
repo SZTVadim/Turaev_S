6f73df50c7a97a1f2addfa80e5343ac4085d35e8
turaev_sv <turaev.prof@gmail.com>
2026-09-02 22:18:53 +0300

    hw7

---
+++ Homeworks/Homework_7/h_work_7.py
@@ -0,0 +1,32 @@
+fruits = {"яблоко", "банан"}
+fruits_2 = {"груша", "виноград"}
+
+fruits.add("апельсин")
+print(fruits, "\n")
+
+fruits.update(fruits_2)
+print(fruits, "\n")
+
+fruits.discard("банан")
+print(fruits, "\n")
+
+fruits.discard("киви")
+print(fruits, "\n")
+
+# fruits.remove("киви")
+# print(fruits, "\n")
+
+fruits.pop()
+print(fruits, "\n")
+
+
+coord = (10, 20, 30, 20, 10, 20, 40)
+print(coord)
+print(coord[0])
+print(coord[-1])
+print(coord[1:4])
+print(30 in coord)
+print(coord.index(20))
+print(coord.count(20))
+print(coord.count(50))
+print(len(coord))
